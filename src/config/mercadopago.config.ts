ab132eec77656b82d3b78e786842c5f9c9852d0f
Jonatan Colussi <jonatancolussi81@gmail.com>
2025-09-29 14:02:13 -0300

    remove MP keys

--- src/config/mercadopago.config.ts
+++ src/config/mercadopago.config.ts
@@ -1,9 +1,9 @@
 import { registerAs } from '@nestjs/config';
 
 export default registerAs('mercadopago', () => ({
-  accessToken: process.env.MERCADOPAGO_ACCESS_TOKEN || 'APP_USR-7639895969324989-092908-29a76b93a0570b89017f6f04903fe8ab-2720512788',
-  publicKey: process.env.MERCADOPAGO_PUBLIC_KEY || 'APP_USR-fe8040ce-fd1c-46b0-8400-14b046ad80f8',
-  webhookUrl: process.env.MERCADOPAGO_WEBHOOK_URL || 'https://your-domain.ngrok.io/api/webhooks/mercadopago/notifications',
+  accessToken: process.env.MERCADOPAGO_ACCESS_TOKEN,
+  publicKey: process.env.MERCADOPAGO_PUBLIC_KEY,
+  webhookUrl: process.env.MERCADOPAGO_WEBHOOK_URL,
   environment: process.env.NODE_ENV === 'production' ? 'production' : 'sandbox',
   timeout: parseInt(process.env.MERCADOPAGO_TIMEOUT || '5000'),
   retries: parseInt(process.env.MERCADOPAGO_RETRIES || '3'),
